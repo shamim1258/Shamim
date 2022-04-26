a7a0ea4bd634aa25c32d4945ee22cf809f0a2d21
shamim1258 <98603543+shamim1258@users.noreply.github.com>
2022-04-26 10:31:52 +0530

    Create base.m

---
+++ Interview/Questions/base.m
@@ -0,0 +1,11 @@
+# Questions Links
+
+[Python Questions Preperations](python_ques.md)  
+[Django Questions Preperations](python_ques.md)  
+[AWS Questions Preperations](python_ques.md)  
+[Shell Scripting Questions Preperations](python_ques.md)    
+
+
+[Python New Link](python_ques.md)  
+[Python New Link](python_ques.md)  
+[Python New Link](python_ques.md)  
